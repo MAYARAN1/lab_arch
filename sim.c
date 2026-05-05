6cbd868b4c49fd8a2eaae8857261d03ff5c7e0f4
Maya <ranbashusha@eng.tau.ac.il>
2026-05-05 15:34:22 +0300

    no error sim

--- sim.c
+++ sim.c
@@ -58,6 +58,7 @@ void fetch_decode_execute(SimulatorState* sim, FILE* trace_file) {
     uint32_t inst = sim->memory[sim->PC];
     uint16_t imm16 = inst & 0xFFFF;
     uint32_t imm32 = (uint32_t)(int32_t)(int16_t)imm16;
+  
 
     // Decode
     uint8_t opcode = (inst >> 25) & 0x1F;
@@ -66,7 +67,8 @@ void fetch_decode_execute(SimulatorState* sim, FILE* trace_file) {
     uint8_t src1 = (inst >> 16) & 0x7;
 
     uint32_t* R = sim->registers;
-
+    sim->registers[1] = imm32; // Load immediate value into R[1]
+    
     // Resolve source values 0 is 0, 1 is immediate
     uint32_t val_src0 = (src0 == 0) ? 0 : (src0 == 1) ? imm32 : R[src0];
     uint32_t val_src1 = (src1 == 0) ? 0 : (src1 == 1) ? imm32 : R[src1];
@@ -122,34 +124,41 @@ void update_traces(SimulatorState* sim, FILE* trace_file, uint16_t current_pc, u
 
     const char* op_name = (opcode <= 24) ? opcode_names[opcode] : "UNKNOWN";
 
+     uint32_t* R = sim->registers;
+     
     // Instruction cycle @ PC
     fprintf(trace_file, "--- instruction %d (%04x) @ PC %d (%04x) -----------------------------------------------------------\n",
         sim->cycle, sim->cycle, current_pc, current_pc);
 
     // Instruction breakdown
-    fprintf(trace_file, "pc = %04x, inst = %08x, opcode = %d (%s), dst = %d, src0 = %d, src1 = %d, immediate = %08x \n",
+    fprintf(trace_file, "pc = %04d, inst = %08x, opcode = %d (%s), dst = %d, src0 = %d, src1 = %d, immediate = %08x\n",
         current_pc, inst, opcode, op_name, dst, src0, src1, imm32);
 
     // Registers 0-3
     fprintf(trace_file, "r[0] = %08x r[1] = %08x r[2] = %08x r[3] = %08x \n",
-        save_regs[0], save_regs[1], save_regs[2], save_regs[3]);
+        save_regs[0], R[1], save_regs[2], save_regs[3]);
 
     // Registers 4-7
     fprintf(trace_file, "r[4] = %08x r[5] = %08x r[6] = %08x r[7] = %08x \n",
         save_regs[4], save_regs[5], save_regs[6], save_regs[7]);
     
     //execution result
-    if (opcode <=  8) {  //LD and ALU ops
+    if (opcode <=  7) {  // ALU ops
         fprintf(trace_file, "\n>>>> EXEC: R[%d] = %d %s %d <<<<\n", 
-                dst, sim->registers[dst], op_name, (src1 == 1 ? imm32 : save_regs[src1]));
-    } else if (opcode == 9) { // ST 
-        fprintf(trace_file, "\n>>>> EXEC: MEM[%d] = %d %s %d <<<<\n", 
-                sim->registers[src1], sim->registers[src0], op_name, sim->registers[src1]);
+                dst, (src0 == 1) ? imm32 : save_regs[src0], op_name, (src1 == 1 ? imm32 : save_regs[src1]));
+    }else if (opcode == 8) { // LD
+        fprintf(trace_file, "\n>>>> EXEC: R[%d] = MEM[%d] = %08x <<<<\n", 
+                dst, sim->registers[src1], sim->memory[sim->registers[src1]]);
+    }else if (opcode == 9) { // ST 
+        fprintf(trace_file, "\n>>>> EXEC: MEM[%d] = R[%d] = %08x <<<<\n", 
+                sim->registers[src1], src0, sim->registers[src0]);
     } else if (opcode >= 16 && opcode <= 19){//jumps
             fprintf(trace_file, "\n>>>> EXEC: %s %d, %d, %d <<<<\n", op_name, 
-            sim->registers[src0], sim->registers[src1], imm32); 
+            sim->registers[src0], sim->registers[src1], sim->PC); 
     }else if (opcode == 24) { // HLT
-        fprintf(trace_file, "\n>>>> EXEC: HALT at PC %04x <<<<\n", current_pc);
+        fprintf(trace_file, "\n>>>> EXEC: HALT at PC %04x<<<<\n", current_pc);\
+        fprintf(trace_file, "sim finished at pc %d, %d instructions", sim->PC, sim->cycle + 1);
+        return;
     }else 
         fprintf(trace_file, "\n>>>> EXEC: %s <<<<\n", op_name);
     
@@ -202,8 +211,6 @@ int main(int argc, char* argv[]) {
 
     // Run the simulation
     run_simulator(&sim, trace_file);
-
-    fprintf(trace_file, "sim finished at pc %d, %d instructions\n", sim.PC, sim.cycle);
 
     // Close trace file and write final memory state
     fclose(trace_file);
